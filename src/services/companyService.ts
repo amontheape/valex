5da0ce870aa90db9b99b5dafa50d7d2d7f5b4382
Liaess <yann.melo@gmail.com>
2022-04-27 10:07:19 -0300

    Preparando revisão de código

--- src/services/companyService.ts
+++
@@ -1,8 +0,0 @@
-import * as companyRepository from '../repositories/companyRepository.js';
-
-export async function validateKey(apiKey: string) {
-  const company = await companyRepository.findByApiKey(apiKey);
-  if(!company) throw { type: 'unauthorized', message: 'Invalid api key'};
-
-  return company;
-}
